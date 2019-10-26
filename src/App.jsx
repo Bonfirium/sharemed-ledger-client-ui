bbfdad09b54f34fa1bf2d2e081ca9ec2bed89042
toffick <gor_nick987@mail.ru>
2019-10-26 17:59:20 +0300

    add patient styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
 import Patient from './patient';
 import Doctor from './doctor';
 
 export default class App extends React.Component {
     render() {
       return (
-        true ? <Patient/> : <Doctor />
+
+              true ? <Patient/> : <Doctor />
+
       );
     }
 }
